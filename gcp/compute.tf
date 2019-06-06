e5745c6bce82315c7e35a3bd1664cc35e74165f2
Ashish Pandey <tech4abetterfuture@gmail.com>
2019-06-06 09:16:22 +0530

    Create compute.tf

---
+++ gcp/compute.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  credentials = "${file("account.json")}"
+  project     = "zeke-2415xx"
+  region      = "asia-east1"
+}
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  tags = ["foo", "bar"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  // Local SSD disk
+  scratch_disk {
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  metadata = {
+    foo = "bar"
+  }
+  metadata_startup_script = "echo hi > /test.txt"
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
