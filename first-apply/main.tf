69ba5eebc57046a9a24b944e4153c0257b426cf3
Ashish Pandey <tech4abetterfuture@gmail.com>
2021-05-22 16:55:49 +0530

    Update main.tf

--- first-apply/main.tf
+++ first-apply/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.11.0"
+    }
+  }
+}
 # Download the ghost:latest docker_image "image_id" to the system using a Terraform resource
 resource "docker_image" "image_id" {
   name = "docker.io/nginx:latest"
