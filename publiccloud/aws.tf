eded4fa1e42c52a3deae05ba4c77ac7fd7908f80
root <root@ip-172-31-37-255.ap-south-1.compute.internal>
2021-05-25 10:12:17 +0000

    terraform.tfvars

--- publiccloud/aws.tf
+++ publiccloud/aws.tf
@@ -19,7 +19,10 @@ terraform  {
 provider "aws" {
   region = "ap-south-1"
 }
-
+resource "aws_key_pair" "ashish-key" {
+	key_name = "ashish-key"
+	public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4+JnFsSev5m8YV0iJ5wOIEGtM2by40ELFTPi8ugjIw1ij8nFermg2XGOnRwDee0C1zY8aEQ0Wg5XDvpfwpds1ENSuQviiJVu29db+CXbneXyXDRPhb/y9xj+3aZnzsgZQNGULyAaoW0DzKKZ/VcyylgcYTB10uHuHTUkFvStqaDJyXEC0o7cBZ9zgu/zPeDY0Zv4uPqs+OZaQrCsLQbpBxcW5PU6E8J/WQVjUzv+1FHzccsvHRjeO2JLB6iEMoivSaGdEIPNpcwERmERtIpahDD53lyCVSBtXw1sQc9gpRlKdsSZEOfxJxX0vXXOhlDDde0GANimm1gOBK9DdzTrx root@ip-172-31-37-255.ap-south-1.compute.internal"
+	}
 resource "aws_instance" "web"{
 	ami = "ami-010aff33ed5991201"
 	instance_type = "t2.micro"
